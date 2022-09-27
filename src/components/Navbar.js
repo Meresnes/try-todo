5149b02e6648d81a4fa4cc0994d17862b822c187
Meresnes <Meresnes@yandex.ru>
2022-09-27 14:31:21 +0300

    addSummForCart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./navbar.css"
 
 
-export default class Navbar extends React.Component {
+export default function Navbar(props) {
+    const [cartSumm, setSumm] = useState(0)
+    useEffect(() => {
+        let itemsSumm = 0
+        props.cartItems.forEach(item => {
+            itemsSumm += item.price
+        })
+        setSumm(itemsSumm)
+    }, [props.cartItems])
+    return (
+        <div className="navbar-content">
 
-
-    render() {
-
-        return (
-            <div className="navbar-content">
-
-                <div className="logo-block">
-                    <img src="./mylogo.png" width={75} height={75} alt="logo" />
-                    <h4>Fake store</h4>
-                </div>
-                <div className="user-block">
-                    <img src="./cart.png" style={{ cursor: "pointer" }}
-                        width={40} height={40}
-                        onClick={this.props.cartHandler}
-                        alt="cart" />
-                    <b>{this.cartSumm}</b>
-                    <img src="./favorites.png"
-                        style={{ cursor: "pointer" }}
-                        width={40}
-                        height={40}
-                        alt="cart" />
-                </div>
-
+            <div className="logo-block">
+                <img src="./mylogo.png" width={75} height={75} alt="logo" />
+                <h4>Fake store</h4>
+            </div>
+            <div className="user-block">
+                <img src="./cart.png" style={{ cursor: "pointer" }}
+                    width={40} height={40}
+                    onClick={props.cartHandler}
+                    alt="cart" />
+                <b>{cartSumm}$</b>
+                <img src="./favorites.png"
+                    style={{ cursor: "pointer" }}
+                    width={40}
+                    height={40}
+                    alt="cart" />
             </div>
 
-        )
-    }
+        </div>
+
+    )
+
 }
